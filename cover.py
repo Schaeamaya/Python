63d4f4fc1a43838b3cb0fe9820d4bf7407b38bda
Schaeamaya <marisolamaya4@icloud.com>
2024-09-17 21:13:04 -0400

    Add files via upload

---
+++ cover.py
@@ -0,0 +1,85 @@
+#Marisol Amaya
+#cover.py
+#COP 2500 0004
+#9/11/2024
+
+import turtle
+#rectangle for whole field
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(500)
+turtle.left(90)
+turtle.forward(300)
+turtle.left(90)
+turtle.forward(250)
+#lines for center
+turtle.left(90)
+turtle.forward(190)
+turtle.left(90)
+#circle for center
+turtle.circle(45)
+turtle.left(270)
+turtle.forward(110)
+#goal line(left side)
+turtle.left(90)
+turtle.forward(250)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(70)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(70)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(180)
+turtle.left(90)
+turtle.forward(50)
+#getting to second goal line
+turtle.left(90)
+turtle.forward(240)
+turtle.left(90)
+turtle.forward(500)
+#second goal line(right side)
+turtle.left(90)
+turtle.forward(60)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(180)
+turtle.right(90)
+turtle.forward(50)
+#Soccer net(right side)
+turtle.right(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(70)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(70)
+#going to create second smaller circle
+turtle.penup()
+turtle.forward(250)
+turtle.right(90)
+turtle.forward(35)
+turtle.right(90)
+turtle.pendown()
+#smaller circle
+turtle.circle(10)
+#start
+turtle.right(90)
+turtle.forward(130)
+
+
+
+            
+
+
+
